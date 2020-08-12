fcb6653e0337fb728ef7f342d61ac4e5eb36ccde
MaximianoEduardo <edu.maxi.96@gmail.com>
2020-08-12 15:32:11 -0300

    Login Auth by Spotify

--- src/page/landing/landing.tsx
+++ src/page/landing/landing.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import spotifyLogoImg from '../../assets/images/spotify2019-830x350.jpg'
+import { loginUrl } from '../../services/spotify';
 import './index.css'
 
 function Landing() {
     return (
       <div className='landing'>
             <img src={spotifyLogoImg} alt="Spotify Logo"/>
-            <a> login with spotify  </a>
+            <a href={loginUrl}> login with spotify  </a>
       </div>
     );
   }
--- src/services/spotify.tsx
+++ src/services/spotify.tsx
@@ -1,3 +1,22 @@
 // Spotify Endpoint
 
+// Click login
+// Redirect to Spotify login page
+// Redirect to home page after logged in
+
 export const authEndpoint = 'https://accounts.spotify.com/authorize'
+
+const redirectUri = 'http://localhost:3000/'
+
+const clientID = '1b302f8a701f424ea9e9338ab3681300'
+
+// Scopes basic preset what is possible to do (security, and anothers stuffs)
+const scopes = [
+    'user-read-currently-playing',
+    'user-read-recently-played',
+    'user-read-playback-state',
+    'user-top-read',
+    'user-modify-playback-state'
+]
+
+export const loginUrl = `${authEndpoint}?client_id=${clientID}&redirect_uri=${redirectUri}&scope=${scopes.join("%20")}&response_type=token&show_dialog=true`

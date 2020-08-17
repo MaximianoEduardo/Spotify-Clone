42dc75bcc925052980ccdc49133252e5d60c385c
MaximianoEduardo <edu.maxi.96@gmail.com>
2020-08-16 21:23:01 -0300

    Connecting to Spotify Api

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React, { useEffect, useState } from 'react';
 import Landing from './page/landing/landing';
-import { getTokenFromResponse } from './services/spotify';
 import { filterTokenResponse } from './services/spotify';
+import SpotifyWebApi from 'spotify-web-api-js'
+
+
+const spotify = new SpotifyWebApi()
 
 function App() {
 
   const [token, setToken] = useState(null)
-
 
   // Run code base on given condition
   useEffect(() =>{
@@ -14,8 +16,21 @@ function App() {
     const _token = filterTokenResponse()
     window.location.hash = ''
 
-    if (_token){
+    if (_token) {
+
       setToken(_token)
+
+      spotify.setAccessToken(_token) // Given Access To Spotify API
+      
+      // getMe => Get User Account
+
+      spotify.getMe().then(user => {
+
+        console.log('User', user)
+
+      })
+
+
     }
 
   }, []);

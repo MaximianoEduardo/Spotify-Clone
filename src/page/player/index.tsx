dc48b8953ff6885d02091ffa586d255f20bd0085
MaximianoEduardo <edu.maxi.96@gmail.com>
2020-08-17 10:31:23 -0300

    Style structure

--- src/page/player/index.tsx
+++ src/page/player/index.tsx
@@ -1,17 +1,30 @@
 import React from 'react'
-import './index.css'
 import Sidebar from '../../structure/sidebar'
 import Body from '../../structure/body'
+import './index.css'
 
-function Player({ spotify }) {
-    return (
-      <div className="player">
-        <div className="player__body">
-          <Sidebar />
-          <Body />
-        </div>
+
+interface spotifyProps {
+
+  spotify: Array<{
+    user: string
+  }>
+
+}
+
+
+const Player: React.FC<spotifyProps> = ({ spotify, ...rest }) => {
+
+  return (
+    <div className="player">
+      <div className="player__body">
+        <Sidebar />
+        <Body />
       </div>
-    );
-  }
+    </div>
+  );
+
+}
+  
   
 export default Player;
